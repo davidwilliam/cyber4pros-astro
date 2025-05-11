112e0a4419da12a61b092c4ec17c93929c4df165
David William Silva <contact@davidwsilva.com>
2025-05-11 12:44:11 -0600

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icons from 'astro-icon';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 
